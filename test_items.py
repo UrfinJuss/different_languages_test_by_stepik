6c5f6223fee200d07fb079b7a15e72c8178dc580
UrfinJuss <by_pa@icloud.com>
2022-07-17 22:34:56 +0300

    fix bugs

--- test_items.py
+++ test_items.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 
 
@@ -6,6 +8,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_button(browser):
     browser.get(link)
-    browser.implicitly_wait(30)
+    time.sleep(30)
     ans = browser.find_element(By.CSS_SELECTOR, 'button.btn-add-to-basket')
     assert ans.is_displayed(), 'Sorry, you cant'
